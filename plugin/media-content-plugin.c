425c9637b860a63eedd9c397966f578daa817887
Jiyong Min <jiyong.min@samsung.com>
2016-06-21 09:47:40 +0900

    Enable DCM on-demand
    Change-Id: Iad6913ea83ff207f75b684be873fef7ef6684b41
    Signed-off-by: Jiyong Min <jiyong.min@samsung.com>

--- plugin/media-content-plugin.c
+++ plugin/media-content-plugin.c
@@ -461,10 +461,12 @@ int update_end(uid_t uid)
 		return MEDIA_SVC_PLUGIN_ERROR;
 	}
 #endif
+#if 0
 	ret = dcm_svc_request_extract_all(uid);
 	if (ret < 0) {
 		return MEDIA_SVC_PLUGIN_ERROR;
 	}
+#endif
 	return MEDIA_SVC_PLUGIN_ERROR_NONE;
 }
 
